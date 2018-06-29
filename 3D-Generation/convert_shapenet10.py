cde8b52b9281563e773247c825472eb3e1e88155
Joni Juvonen <joni.juvonen@aarila.com>
2018-06-29 08:43:16 +0300

    modified convert_shapenet10.py to run on Windows and altered object classes

--- 3D-Generation/convert_shapenet10.py
+++ 3D-Generation/convert_shapenet10.py
@@ -2,7 +2,8 @@ import numpy as np
 import sys
 import os
 import scipy.io
-from path import Path
+import glob
+#from path import Path
 from tqdm import tqdm
 
 if sys.argv[-1] == '-v': # this will allow you to visualize the models as they are made, more of a sanity check 
@@ -14,16 +15,16 @@ if sys.argv[-1] == '-v': # this will allow you to visualize the models as they a
 
 instances = {}
 class_id_to_name = {
-    "1": "bathtub",
-    "2": "bed",
+    "1": "cup",
+    "2": "monitor",
     "3": "chair",
     "4": "desk",
-    "5": "dresser",
-    "6": "monitor",
-    "7": "night_stand",
-    "8": "sofa",
+    "5": "car",
+    "6": "bowl",
+    "7": "vase",
+    "8": "lamp",
     "9": "table",
-    "10": "toilet"
+    "10": "person"
 }
 class_name_to_id = { v : k for k, v in class_id_to_name.items() }
 class_names = set(class_id_to_name.values())
@@ -33,36 +34,45 @@ if not os.path.exists('data/train/'):
     os.makedirs('data/train/')
 if not os.path.exists('data/test/'):
     os.makedirs('data/test/')
-base_dir = Path(sys.argv[1]).expand()
+base_dir = (sys.argv[2])
 
-for fname in tqdm(sorted(base_dir.walkfiles('*.mat'))):
-    if fname.endswith('test_feature.mat') or fname.endswith('train_feature.mat'): 
-        continue
-    elts = fname.splitall()
-    info = Path(elts[-1]).stripext().split('_')
-    if len(info)<3: continue  
-    if info[0] == 'discriminative' or info[0] == 'generative' : continue 
-    instance = info[1]
-    rot = int(info[2])
-    split = elts[-2]
-    classname = elts[-4].strip()
-    if classname in class_names:
-        dest = 'data/'+split+'/' + classname + '/'
-        if not os.path.exists(dest):
-            os.makedirs(dest)
-        arr = scipy.io.loadmat(fname)['instance'].astype(np.uint8)
-        matrix = np.zeros((32,)*3, dtype=np.uint8)
-        matrix[1:-1,1:-1,1:-1] = arr
-        if sys.argv[-1] == '-v':
-            xx, yy, zz = np.where(matrix>= 0.3)
-            mayavi.mlab.points3d(xx, yy, zz,
+for (dirpath, dirnames, filenames) in tqdm(os.walk(base_dir)):
+    for filename in filenames:
+        if filename.endswith('.mat'):
+            fname = os.path.join(dirpath,filename)
+            if fname.endswith('test_feature.mat') or fname.endswith('train_feature.mat'): 
+                continue
+            elts = os.path.split(fname) #fname.splitall()
+            nameonly = os.path.splitext(elts[1])
+            info = nameonly[0].split('_')
+            if len(info)<3: continue  
+            if info[0] == 'discriminative' or info[0] == 'generative' : continue 
+            instance = info[1]
+            rot = int(info[2])
+            dirs = dirpath.split('\\')					
+            split = dirs[len(dirs) - 1]
+            classname = dirs[len(dirs) - 3].strip()
+            if classname in class_names:
+                dest = 'data/'+split+'/' + classname + '/'
+            else:
+                continue
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+            arr = scipy.io.loadmat(fname)['instance'].astype(np.uint8)
+            matrix = np.zeros((32,)*3, dtype=np.uint8)
+            matrix[1:-1,1:-1,1:-1] = arr
+            if sys.argv[-1] == '-v':
+                xx, yy, zz = np.where(matrix>= 0.3)
+                mayavi.mlab.points3d(xx, yy, zz,
                                  
-                                 color=(.1, 0, 1),
-                                 scale_factor=1)
+                                     color=(.1, 0, 1),
+                                     scale_factor=1)
 
-            mayavi.mlab.show()
-        # saves the models by instance name, and then rotation 
-        np.save(dest +  instance + '_' + str(rot) , matrix)
-        
+                mayavi.mlab.show()
+            # saves the models by instance name, and then rotation 
+            np.save(dest +  instance + '_' + str(rot) , matrix)
+        else:
+            continue
+ 
 
 
